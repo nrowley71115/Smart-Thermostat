ed3b4baf95ae0637a8979c677c586703b41e4452
Nathan Rowley <nrowley71115@gmail.com>
2023-09-19 18:32:46 -0500

    built html nav bar, bootstrap, style.css, schedule.html

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_world():
+def index():
     if request.method == 'POST':
         return "update temperautre"
     
     # TODO get current temp and pass to index
     return render_template("index.html")
+
+
+@app.route("/statistics")
+def statistics():
+    return render_template("statistics.html")
+
+
+@app.route("/schedule")
+def schedule():
+    # TODO Implement schedule page
+    return render_template("schedule.html")
