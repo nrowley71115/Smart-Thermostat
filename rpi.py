ba6ec572acd19ebdd2ff3065ccc42104fab8a24f
nrowley71115 <nrowley71115@gmail.com>
2023-09-23 15:59:12 -0500

    made temp read on LCD functioning

--- rpi.py
+++ rpi.py
@@ -14,20 +14,6 @@ from rpi_lcd import LCD
 import RPi.GPIO as GPIO
 
 
-# Initialize DHT22
-def init_dht22():
-   dhtDevice = adafruit_dht.DHT22(board.D16, use_pulseio=False)
-
-# Initialize LCD
-def init_lcd():
-   lcd = LCD()
-
-   def safe_exit(signum, frame):
-      exit(1)
-
-   signal(SIGTERM, safe_exit)
-   signal(SIGHUP, safe_exit)
-
 def init_relays():
    # Initialize Relays
    GPIO.setmode(GPIO.BCM)
@@ -40,48 +26,63 @@ def init_relays():
 
 if __name__ == '__main__':
    t = Thermostat()
-   init_dht22()
+   
+   # Initialize DHT22
+   dhtDevice = adafruit_dht.DHT22(board.D16, use_pulseio=False)
+   
+   # Initialize 16x2 LCD Display
+   lcd = LCD()
 
-   while True:
+   def safe_exit(signum, frame):
+      exit(1)
+
+   signal(SIGTERM, safe_exit)
+   signal(SIGHUP, safe_exit)
+   
+   # Initalize Relays
+   init_relays()
+   
+   # Thermostat Control Loop
+   while True: 
       # get schedule and setpoint
       schedule = t.get_schedule()
       setpoint = t.get_schedule_setpoint()
-      setpoint_str = f'Setpoint: {setpoint} F'
+      setpoint_str = f'{setpoint} F'
 
       # get current temp & humidity
       try:
          temperature_c = dhtDevice.temperature
          temperature_f = round(temperature_c * (9/5) + 32, 1)
-         temperature_f_str = f'Temp: {temperature_f} F'
+         temperature_f_str = f'{temperature_f} F'
          humidity = dhtDevice.humidity
       except RuntimeError as error:
-         print(error.args[0])
+         # print(error.args[0])
+         print("DHT error")
          sleep(2)
          continue
       except Exception as error:
          dhtDevice.exit()
          raise error
 
-      # get current system mode
-      ac_mode = "ON"
-      fan_mode = "ON"
-      heat_mode = "ON"
+
+      # TODO check for button input
+
+
+      # TODO update setpoint if necessary
+
+
+      # TODO get current system mode
+      ac_mode = "N"
+      fan_mode = "F"
+      heat_mode = "A"
 
       # write current temp & setpoint to 16x2 LCD
       lcd.text(f"C:{temperature_f_str}  S:{setpoint_str}", 1)
       lcd.text(f"AC:{ac_mode} FAN:{fan_mode} HT:{heat_mode}", 2)
 
 
-
-      # TODO check for button input
-
+      # TODO control the thermostat - ie relays on or off
 
 
-      # TODO update schedule and setpoint if necessary
-
-
-      # TODO control the thermostat
-
-
-      sleep(5)
+      sleep(2)
       print("Thermostat running")
